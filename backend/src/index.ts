bbccab3022bee9be9cb46090b587aad655e6ab72
sudip khatri <sudpkhatri4@gmail.com>
2024-01-09 10:55:15 +1100

    index changed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,10 +29,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
+  cors()
 );
 
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
